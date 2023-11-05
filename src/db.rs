5b280e52a3b5870fcd8df4da6048080cf43e27d8
prowayne <prosmarter@163.com>
2023-11-05 19:29:56 +0800

    use db pool

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,6 @@
-use std::env;
+use deadpool_postgres::Pool;
 use serde::Serialize;
-use tokio_postgres::{NoTls, Error, Client};
+use tokio_postgres::Error;
 
 #[derive(Serialize)]
 pub struct NFT {
@@ -26,20 +26,21 @@ pub struct Resolver {
     pub version: i32
 }
 
-async fn connect() -> Result<Client, Error> {
-    let conn_str = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let (client, connection) = tokio_postgres::connect(&conn_str, NoTls).await?;
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection db err: {}", e)
-        }
-    });
+// use pool client now
+// async fn connect() -> Result<Client, Error> {
+//     let conn_str = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     let (client, connection) = tokio_postgres::connect(&conn_str, NoTls).await?;
+//     tokio::spawn(async move {
+//         if let Err(e) = connection.await {
+//             eprintln!("connection db err: {}", e)
+//         }
+//     });
 
-    Ok(client)
-}
+//     Ok(client)
+// }
 
-pub async fn get_name_by_namehash(name_hash: &str) -> Result<NFTWithPrimary, Error> {
-    let client = connect().await?;
+pub async fn get_name_by_namehash(pool: &Pool, name_hash: &str) -> Result<NFTWithPrimary, Error> {
+    let client = pool.get().await.unwrap();
     let query = "select an.id, an.name_hash, an.full_name, an.resolver, ap.id from ans.ans_name AS an 
             LEFT JOIN ans.ans_primary_name as ap ON an.name_hash = ap.name_hash
             WHERE an.name_hash = $1 limit 1";
@@ -73,8 +74,8 @@ pub async fn get_name_by_namehash(name_hash: &str) -> Result<NFTWithPrimary, Err
     Ok(nft)
 }
 
-pub async fn get_names_by_addr(address: &str) -> Result<Vec<NFTWithPrimary>, Error> {
-    let client = connect().await?;
+pub async fn get_names_by_addr(pool: &Pool, address: &str) -> Result<Vec<NFTWithPrimary>, Error> {
+    let client = pool.get().await.unwrap();
     let query = "select ao.id, ao.name_hash, ao.address, an.full_name, ap.id, an.resolver from ans.ans_nft_owner as ao 
             JOIN ans.ans_name as an ON ao.name_hash = an.name_hash
             LEFT JOIN ans.ans_primary_name as ap ON ao.name_hash = ap.name_hash
@@ -111,8 +112,9 @@ pub async fn get_names_by_addr(address: &str) -> Result<Vec<NFTWithPrimary>, Err
     Ok(nft_list)
 }
 
-pub async fn get_resolvers_by_namehash(name_hash: &str) -> Result<Vec<Resolver>, Error> {
-    let client = connect().await?;
+pub async fn get_resolvers_by_namehash(pool: &Pool, name_hash: &str) -> Result<Vec<Resolver>, Error> {
+    let client = pool.get().await.unwrap();
+
     let query = "select ar.id, ar.category, ar.version, ar.name from ans.ans_resolver As ar
         LEFT JOIN ans.ans_name_version as av ON ar.name_hash = av.name_hash
         WHERE ar.version=av.version and ar.name_hash = $1";
@@ -135,8 +137,30 @@ pub async fn get_resolvers_by_namehash(name_hash: &str) -> Result<Vec<Resolver>,
     Ok(resolver_list)
 }
 
-pub async fn get_subdomains_by_namehash(name_hash: &str) -> Result<Vec<NFT>, Error> {
-    let client = connect().await?;
+pub async fn get_resolver(pool: &Pool, name_hash: &str, category: &str) -> Result<Resolver, Error> {
+    let client = pool.get().await.unwrap();
+
+    let query = "select ar.id, ar.category, ar.version, ar.name from ans.ans_resolver As ar
+        LEFT JOIN ans.ans_name_version as av ON ar.name_hash = av.name_hash
+        WHERE ar.version=av.version and ar.name_hash = $1 and ar.category = $2 limit 1";
+    println!("get_resolvers_by_namehash query db: {} name_hash {}, category {}", &query, &name_hash, &category);
+
+    let row = client.query_one(query, &[&name_hash, &category]).await?;
+    
+    let resolver = Resolver {
+        name_hash: name_hash.to_string(),
+        category: row.get(1),
+        name: row.get(3),
+        version: row.get(2),
+    };
+
+    Ok(resolver)
+}
+
+pub async fn get_subdomains_by_namehash(pool: &Pool, name_hash: &str) -> Result<Vec<NFT>, Error> {
+    // let client = connect().await?;
+    let client = pool.get().await.unwrap();
+
     let query = "select an.id, an.name_hash, an.name, ao.address from ans.ans_name as an
             LEFT JOIN ans.ans_nft_owner as ao ON an.name_hash = ao.name_hash
                 WHERE parent = $1";
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use actix_cors::Cors;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use deadpool_redis::{Config, Runtime, Pool};
+use snarkvm_console_program::FromStr;
+use tokio_postgres::NoTls;
 use std::env;
-
 
 mod utils;
 mod client;
@@ -57,9 +58,9 @@ async fn name_to_hash(name: web::Path<String>) -> impl Responder {
 }
 
 #[get("/hash_to_name/{name_hash}")]
-async fn hash_to_name(_pool: web::Data<Pool>, name_hash: web::Path<String>) -> impl Responder {
+async fn hash_to_name(db_pool: web::Data<deadpool_postgres::Pool>, name_hash: web::Path<String>) -> impl Responder {
     let name_hash = name_hash.into_inner();
-    let nft = db::get_name_by_namehash(&name_hash).await;
+    let nft = db::get_name_by_namehash(&db_pool, &name_hash).await;
 
     match nft {
         Ok(data) => HttpResponse::Ok().json(NameHash {name_hash: data.name_hash, name: data.name}),
@@ -91,23 +92,32 @@ async fn address_api(name: web::Path<String>) -> impl Responder {
 
 
 #[get("/resolver")]
-async fn resolver(resolver_params: web::Query<GetResolverParams>) -> impl Responder {
+async fn resolver(db_pool: web::Data<deadpool_postgres::Pool>, resolver_params: web::Query<GetResolverParams>) -> impl Responder {
     let name = resolver_params.name.clone();
     let category = resolver_params.category.clone();
     println!("name: {}, category: {}", name, category);
-    let content = client::get_resolver(&category, &name).await;
 
-    match content {
-        Ok(content) => HttpResponse::Ok().json(ResolverContent { content, name: name.clone() , category: category.clone()}),
+    let name_hash = utils::parse_name_hash(&name);
+    let name_hash = match name_hash {
+        Ok(name_hash_field) => name_hash_field.to_string(),
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(serde_json::json!({ "error": format!("Error parsing name: {}", e) }));
+        }
+    };
+
+    let nft = db::get_resolver(&db_pool, &name_hash, &category).await;
+
+    match nft {
+        Ok(nft) => HttpResponse::Ok().json(ResolverContent { content: nft.name, name: name.clone() , category: nft.category}),
         Err(_e) => HttpResponse::NotFound().finish(),
     }
 }
 
 
 #[get("/public_ans/{address}")]
-async fn public_ans(address: web::Path<String>) -> impl Responder {
+async fn public_ans(db_pool: web::Data<deadpool_postgres::Pool>, address: web::Path<String>) -> impl Responder {
     let address = address.into_inner();
-    let names = db::get_names_by_addr(&address).await;
+    let names = db::get_names_by_addr(&db_pool, &address).await;
 
     match names {
         Ok(data) => HttpResponse::Ok().json(data),
@@ -116,7 +126,7 @@ async fn public_ans(address: web::Path<String>) -> impl Responder {
 }
 
 #[get("/resolver/{name}")]
-async fn resolvers(name: web::Path<String>) -> impl Responder {
+async fn resolvers(db_pool: web::Data<deadpool_postgres::Pool>, name: web::Path<String>) -> impl Responder {
     let name = name.into_inner();
 
     let name_hash = utils::parse_name_hash(&name);
@@ -127,7 +137,7 @@ async fn resolvers(name: web::Path<String>) -> impl Responder {
         }
     };
 
-    let name_resolvers = db::get_resolvers_by_namehash(&name_hash).await;
+    let name_resolvers = db::get_resolvers_by_namehash(&db_pool, &name_hash).await;
 
     match name_resolvers {
         Ok(data) => HttpResponse::Ok().json(data),
@@ -137,7 +147,7 @@ async fn resolvers(name: web::Path<String>) -> impl Responder {
 
 
 #[get("/subdomain/{name}")]
-async fn subdomains(name: web::Path<String>) -> impl Responder {
+async fn subdomains(db_pool: web::Data<deadpool_postgres::Pool>, name: web::Path<String>) -> impl Responder {
     let name = name.into_inner();
 
     let name_hash = utils::parse_name_hash(&name);
@@ -148,7 +158,7 @@ async fn subdomains(name: web::Path<String>) -> impl Responder {
         }
     };
 
-    let name_subdomains = db::get_subdomains_by_namehash(&name_hash).await;
+    let name_subdomains = db::get_subdomains_by_namehash(&db_pool, &name_hash).await;
 
     match name_subdomains {
         Ok(data) => HttpResponse::Ok().json(data),
@@ -160,10 +170,16 @@ async fn subdomains(name: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1:6379/0".to_string());
+    let redis_cfg = Config::from_url(redis_url);
+    let redis_pool = redis_cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
 
-    let cfg = Config::from_url(redis_url);
-
-    let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
+    let db_url = env::var("DATABASE_URL").unwrap_or_else(|_| "postgresql://casaos:casaos@10.0.0.17:5432/aleoe".to_string());
+    let db_config= tokio_postgres::Config::from_str(&db_url).unwrap();
+    let mgr_config =deadpool_postgres::ManagerConfig {
+        recycling_method: deadpool_postgres::RecyclingMethod::Fast
+    };
+    let db_mgr = deadpool_postgres::Manager::from_config(db_config, NoTls, mgr_config);
+    let db_pool = deadpool_postgres::Pool::builder(db_mgr).max_size(16).build().unwrap();
 
     HttpServer::new(move || {
         App::new()
@@ -171,7 +187,8 @@ async fn main() -> std::io::Result<()> {
                 Cors::permissive()
                     .allow_any_origin()
             )
-            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(redis_pool.clone()))
+            .app_data(web::Data::new(db_pool.clone()))
             .service(name_to_hash)
             .service(hash_to_name)
             .service(name_api)
